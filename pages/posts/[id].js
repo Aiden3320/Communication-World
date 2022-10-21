2068ce371156c7d55309796e1e4abe2c32903f70
SuperDev0601 <superdev0601@outlook.com>
2022-10-20 17:27:35 -0700

    Structure Changed

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout';
+import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 export async function getStaticProps({ params }) {
